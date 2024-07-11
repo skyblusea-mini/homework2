6a0881da9e511aad9028d372ffa9278831ad89c8
김민지 <skyblusea@minirecord.com>
2024-07-11 14:32:27 +0900

    [Design] 기본 레이아웃 설정
    (cherry picked from commit d180c41406a881537b9dccf66585e086c3e9e2ef)

--- src/common/layout/MainLayout.tsx
+++ src/common/layout/MainLayout.tsx
@@ -1,12 +1,36 @@
+import styled from '@emotion/styled'
+import { Button } from '@mui/joy'
 import { Outlet } from 'react-router-dom'
 
 /** 메인 공통 레이아웃 */
 const MainLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <AppContainer>
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
+    </AppContainer>
   )
 }
 
 export default MainLayout
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  max-width: var(--max-w);
+  display: flex;
+  `
+
+/** AppBar 80px, BottomNav 60px로 설정
+ *
+*/
+const MainContainer = styled.main`
+  display: flex;
+  flex:1;
+  padding-top: var(--header-h);
+`
